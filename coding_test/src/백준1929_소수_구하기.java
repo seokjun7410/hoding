2d72157fa2f3b1b3d3e4505f0467eed20d376dff
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-09 12:09:23 +0900

    CodingTest: 백준1929_잃어버린_괄호/fix

--- coding_test/src/백준1929_소수_구하기.java
+++ coding_test/src/백준1929_소수_구하기.java
@@ -7,11 +7,11 @@ public class 백준1929_소수_구하기 {
         int M = scanner.nextInt();
         int N = scanner.nextInt();
         int []A = new int[N+1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 2; i <= N; i++) {
             A[i] = i;
         }
 
-        for (int i = 2; i <Math.sqrt(N); i++) {
+        for (int i = 2; i <=Math.sqrt(N); i++) {
             if(A[i]==0) continue;
             for (int j = i+i; j <=N; j=j+i) {
                 A[j] = 0;
