49f892684d981836a0bd22e98634a032d08358ef
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-13 17:19:57 +0900

    feat: create project #2

---
+++ book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/SpringbootInPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_in_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInPracticeApplication.class, args);
+    }
+
+}
---
+++ book/springboot_in_practice/src/test/java/com/example/springboot_in_practice/SpringbootInPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_in_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
