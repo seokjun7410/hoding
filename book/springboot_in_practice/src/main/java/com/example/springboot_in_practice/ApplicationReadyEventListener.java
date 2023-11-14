392e58bfac1453579f2355823b71fe087ae65830
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-14 14:15:43 +0900

    fix: rename

--- book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/ApplicationReadyEventListener.java
+++ book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/ApplicationReadyEventListener.java
@@ -2,7 +2,6 @@ package com.example.springboot_in_practice;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +9,7 @@ import java.time.LocalDateTime;
 
 
 @Service
-public class EventService {
+public class ApplicationReadyEventListener {
 
     //애플리케이션이 요청을 처리할 준비가 됐을때 발행된다. 초기화가 완료된 시점임으로 애플리케이션 내부상태를 변경하는 것은 권장되지 않는ㄷ.ㅏ
     @EventListener(ApplicationReadyEvent.class)
