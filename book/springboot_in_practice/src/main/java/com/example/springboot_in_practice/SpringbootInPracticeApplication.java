917bc5db94d7510066212abeb7f66a2ac3641e40
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-14 13:55:09 +0900

    feat: SpringApplication 직접생성하여 리액티브타 apllication 시동 #2

--- book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/SpringbootInPracticeApplication.java
+++ book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/SpringbootInPracticeApplication.java
@@ -1,13 +1,20 @@
 package com.example.springboot_in_practice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootInPracticeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootInPracticeApplication.class, args);
+    public static void main(String[] args)
+    {
+//        SpringApplication.run(SpringbootInPracticeApplication.class, args);
+
+        //SpringApplication 직접생성
+        SpringApplication springApplication = new SpringApplication(SpringbootInPracticeApplication.class);
+        springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+        springApplication.run(args);
     }
 
 }
