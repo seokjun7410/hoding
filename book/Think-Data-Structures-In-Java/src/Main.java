0eb5879a46590ac393c2cb2b9d13f94d382f444e
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-15 17:52:39 +0900

    feat: 구현체 자료구조 변경 테스트

--- book/Think-Data-Structures-In-Java/src/Main.java
+++ book/Think-Data-Structures-In-Java/src/Main.java
@@ -1,9 +1,11 @@
+import list_client_example.MyListClientExample;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        ListClientExample listClientExample = new ListClientExample();
+        MyListClientExample listClientExample = new MyListClientExample();
         List list = listClientExample.getList();
         System.out.println(list);
     }
--- book/Think-Data-Structures-In-Java/src/list_client_example/MyListClientExample.java
+++ book/Think-Data-Structures-In-Java/src/list_client_example/MyListClientExample.java
@@ -1,3 +1,5 @@
+package list_client_example;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -5,10 +7,10 @@ import java.util.List;
  *  구체적 자료구조를 노출하지않고 추상화한 클래스입니다.
  *  필요에따라 LinkedList를 다른 자료구조료 변경하더라도 해당객체를 사용하는 외부의 변화 방지할 수 있습니다.
  */
-public class ListClientExample {
+public class MyListClientExample {
     private List list;
 
-    public ListClientExample(){
+    public MyListClientExample(){
         list = new LinkedList<>();
     }
 
