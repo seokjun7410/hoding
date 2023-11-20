0f95484c97b9ce1870d224979a0f8f39246ab17e
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-20 20:33:27 +0900

    refactor: Theater.enter 세부사항을 TicketSeller의 책임으로 변경한다. #9

--- book/object/src/TicketSeller.java
+++ book/object/src/TicketSeller.java
@@ -4,12 +4,25 @@
  */
 public class TicketSeller {
     private TicketOffice ticketOffice;
+    private TicketSeller ticketSeller;
 
     public TicketSeller(final TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
+
+    public void sellTo(Audience audience){
+        if(audience.getBag().hasInvitation()){
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        }else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
