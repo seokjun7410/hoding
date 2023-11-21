5f6383ebbe406e51ed622f20ea5d86a220458e7f
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-21 14:07:25 +0900

    feat: SpringApplication에서 property설정 #16

--- book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/SpringbootInPracticeApplication.java
+++ book/springboot_in_practice/src/main/java/com/example/springboot_in_practice/SpringbootInPracticeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 /**
  * @SpringBootApplication
  *  - @SpringBootConfiguration : 빈 관리 설정 클래스에 붙히고 설정과정에 참여한다.
@@ -21,6 +23,12 @@ public class SpringbootInPracticeApplication {
         SpringApplication springApplication = new SpringApplication(SpringbootInPracticeApplication.class);
         springApplication.setWebApplicationType(WebApplicationType.SERVLET);
         springApplication.addListeners(new ApplicationStartingEventListener());
+
+        //SpringApplication 에서 property 설정
+        Properties properties = new Properties();
+        properties.setProperty("spring.config.on-not-found","ignore"); //설정파일이 존재하지 않아도 예외를 발생시키지 않는다.
+        springApplication.setDefaultProperties(properties);
+
         springApplication.run(args);
     }
 
