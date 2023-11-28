2ee11b737d79ccf6a3dbc6a407bc7396547005b9
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-28 13:42:49 +0900

    copy: MyLinkedList Practice2 #31

---
+++ book/Think-Data-Structures-In-Java/src/practice3/MyLinkedListTest.java
@@ -0,0 +1,33 @@
+package practice3;
+
+/**
+ *
+ */
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import practice2.MyArrayListTest;
+
+
+/**
+ * @author downey
+ *
+ */
+public class MyLinkedListTest extends MyArrayListTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        mylist = new MyLinkedList<Integer>();
+        mylist.addAll(list);
+    }
+}
