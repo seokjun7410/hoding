4e258c27fbfa063f51cd662cb57d08f2c8668523
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-12-05 14:21:49 +0900

    fix: Screening ADD calculateFee #47

--- book/object/src/chapter4/Screening.java
+++ book/object/src/chapter4/Screening.java
@@ -1,11 +1,27 @@
 package chapter4;
 
+import movie_reservation.domain.Money;
+
 import java.time.LocalDateTime;
 
 public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreend;
+
+    public Money calculateFee(int audienceCount){
+        switch (movie.getMovieType()){
+            case AMOUNT_DISCOUNT:
+                if(movie.isDiscountable(whenScreend,sequence)){
+                    return movie.calculatePercentDiscountedFee().multiply(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:return null; //생략
+            case NONE_DISCOUNT: return null; //생략
+        }
+
+        return movie.calculateNoneDiscountedFee();
+    }
 
     public void setMovie(final Movie movie) {
         this.movie = movie;
