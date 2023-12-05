2b99d88845e625e3a463a19f1652866dededd492
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-12-05 14:22:58 +0900

    fix: ReservationAgency.reserve #47

--- book/object/src/chapter4/ReservationAgency.java
+++ book/object/src/chapter4/ReservationAgency.java
@@ -5,35 +5,7 @@ import movie_reservation.domain.Money;
 public class ReservationAgency {
 
     public Reservation reserve(Screening screening, Customer customer, int audienceCount){
-        Movie movie = screening.getMovie();
-
-        boolean discountable = false;
-        for (DiscountCondition condition : movie.getDiscountConditions()) {
-            if(condition.getType().equals(DiscountConditionType.PERIOD)){
-                //생략
-            }else{//DiscountConditionType.SEQUENCE 알경우
-                //생략
-            }
-
-            if(discountable){
-                break;
-            }
-        }
-
-        Money fee;
-        if(discountable){
-            Money dicountAmount = Money.ZERO;
-            switch (movie.getMovieType()){ //생략
-                case AMOUNT_DISCOUNT : break;
-                case PERCENT_DISCOUNT:  break;
-                case NONE_DISCOUNT: break;
-            }
-
-            fee = movie.getFee().minus(dicountAmount);
-        }else {
-            fee = movie.getFee();
-        }
-
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer,screening,fee,audienceCount);
     }
 }
