c446490323c287a1d5e9c176a70f7451c8b0eedb
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-12 09:49:56 +0900

    CodingTest: inf_바둑이승차

---
+++ coding_test/src/inf_바둑이_승차/inf_바둑이승차.java
@@ -0,0 +1,42 @@
+package inf_바둑이_승차;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_바둑이승차 {
+    static int C;
+    static int max;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        int input[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(br.readLine());
+        }
+
+        DFS(-1, 0, input);
+
+        System.out.println(max);
+    }
+
+    private static void DFS(int L, int sum, int[] input) {
+        if (L+1 >= N || sum > C) {
+            if(sum<=C) {
+                max = Math.max(max, sum);
+            }
+            return;
+        }
+
+        DFS(L+1,sum+input[L+1],input);
+        DFS(L+1,sum,input);
+
+    }
+}
--- coding_test/src/inf_바둑이_승차/inf_바둑이승차_01_09.java
+++ coding_test/src/inf_바둑이_승차/inf_바둑이승차_01_09.java
@@ -1,40 +1,45 @@
+package inf_바둑이_승차;
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class inf_바둑이승차 {
-    static int C;
-    static int max;
+public class inf_바둑이승차_01_09 {
+    static int W;
     static int N;
-
+    static int input[];
+    static int count = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        C = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        int input[] = new int[N];
+        input = new int[N];
         for (int i = 0; i < N; i++) {
             input[i] = Integer.parseInt(br.readLine());
         }
 
-        DFS(-1, 0, input);
+        DFS(0,0);
 
-        System.out.println(max);
+        System.out.println(count);
+
     }
 
-    private static void DFS(int L, int sum, int[] input) {
-        if (L+1 >= N || sum > C) {
-            if(sum<=C) {
-                max = Math.max(max, sum);
-            }
+    private static void DFS(int L, int sum) {
+        if(sum < W){
+            count = Math.max(sum,count);
+        }
+        if(L == N) {
             return;
         }
 
-        DFS(L+1,sum+input[L+1],input);
-        DFS(L+1,sum,input);
 
+        DFS(L+1,sum+input[L]);
+        DFS(L+1,sum);
     }
 }
