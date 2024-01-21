75c2eb60c5093960cf161065c0ee72b58369d1c9
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-21 16:32:32 +0900

    vue3-cli 연습

---
+++ pratice/vuejs/vue3-practice/vue3-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-header 
+  v-bind:appTitle="message"
+  v-on:change="changeMessage"
+  ></app-header>
+  <div>
+    {{message}}
+  </div>
+  <button @click="showAlert">warn</button>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+  export default {
+    components: {
+      'app-header' : AppHeader
+    },
+    data() {
+      return {
+        message: 'app header components'
+      }
+    },
+    methods:{
+      showAlert(){
+        alert('heelo')
+      },
+      changeMessage(){
+        this.message = 'changed'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pratice/vuejs/vue3-practice/vue3-app/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ appTitle }}</h1>
+    <button @click="changeTitle">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["appTitle"],
+  methods: {
+    changeTitle() {
+      this.$emit("change");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
