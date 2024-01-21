db892c852d12a0eadfcae23c305c2364c94668d2
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-21 16:33:05 +0900

    vue3-axios 및 form 연습

---
+++ pratice/vuejs/vue3-practice/vue3-form/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="submitForm">
+      <div>
+        <label for="userId">ID:</label>
+        <input id="userId" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input type="text" v-model="password">
+      </div>
+    <button type="submit">login</button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+    methods: {
+      submitForm(){
+        // event.preventDefault();
+        console.log("fsdafd");
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
