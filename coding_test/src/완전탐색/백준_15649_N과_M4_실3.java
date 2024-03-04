65f30fb2a7a9763d3cf2ce570428ad50e5cf20c4
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-03-04 17:42:35 +0900

    CodingTest:백준_15649_N과_M4_실3

---
+++ coding_test/src/완전탐색/백준_15649_N과_M4_실3.java
@@ -0,0 +1,40 @@
+package 완전탐색;
+
+
+import java.util.Scanner;
+
+public class 백준_15649_N과_M4_실3 {
+
+	private static int N;
+	private static int M;
+	private static int[] arr;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[M];
+		for (int selected = 1; selected < N + 1; selected++) {
+			recursive(selected, M - 1, 0);
+		}
+
+		System.out.println(sb);
+	}
+
+	private static void recursive(int selected, int maxDepth, int currentDepth) {
+		arr[currentDepth] = selected;
+
+		if (currentDepth == maxDepth) {
+			for (int i : arr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = selected; i < N + 1; i++) {
+			recursive(i, maxDepth, currentDepth + 1);
+		}
+	}
+}
